07e6ae664ce3381b15dba9e12cb1b00fda8878a8
Uladzimir Chvei <chveyvladzimir@gmail.com>
2024-02-01 16:05:33 +0300

    fromWork01,02

--- Patients/src/main/java/com/chvei/DoP/services/servicesImp/DiagnosisServiceImp.java
+++ Patients/src/main/java/com/chvei/DoP/services/servicesImp/DiagnosisServiceImp.java
@@ -2,9 +2,12 @@ package com.chvei.DoP.services.servicesImp;
 
 import com.chvei.DoP.DTO.DiagnosisDTO;
 import com.chvei.DoP.entity.Diagnosis;
+import com.chvei.DoP.entity.Patient;
+import com.chvei.DoP.entity.patternsDiseases.MainDisease;
 import com.chvei.DoP.repositories.DiagnosisRepository;
 import com.chvei.DoP.repositories.PatientRepository;
 import com.chvei.DoP.services.DiagnosisService;
+import com.chvei.DoP.services.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +17,21 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Service
+
 public class DiagnosisServiceImp implements DiagnosisService {
     private final Logger logger = Logger.getLogger(DiagnosisServiceImp.class.getName());
     private DiagnosisRepository diagnosisRepository;
     private PatientRepository patientRepository;
+    private PatientService patientService;
 
     public DiagnosisServiceImp() {
     }
 
     @Autowired
-    public DiagnosisServiceImp(DiagnosisRepository diagnosisRepository, PatientRepository patientRepository) {
+    public DiagnosisServiceImp(DiagnosisRepository diagnosisRepository, PatientRepository patientRepository, PatientService patientService) {
         this.diagnosisRepository = diagnosisRepository;
         this.patientRepository = patientRepository;
+        this.patientService = patientService;
     }
 
     @Override
@@ -36,7 +42,7 @@ public class DiagnosisServiceImp implements DiagnosisService {
 
     @Override
     public DiagnosisDTO getDiagnosisDTOByPatientId(Long id) {
-        Diagnosis diagnosis = diagnosisRepository.findByPatient_Id(id).orElseThrow();
+        Diagnosis diagnosis = diagnosisRepository.findByPatient(id).orElseThrow();
         return toDTO(diagnosis);
     }
 
@@ -52,22 +58,29 @@ public class DiagnosisServiceImp implements DiagnosisService {
 
     @Override
     public DiagnosisDTO saveDiagnosis(DiagnosisDTO diagnosisDTO) {
-        Diagnosis diagnosis1 = diagnosisRepository.save(toEntity(diagnosisDTO));
-        System.out.println(diagnosisDTO);
-        System.out.println("saveDiagnosis: " +diagnosis1);
-      //  logger.log(Level.INFO, "Diagnosis " + diagnosis1.getMainDisease().getName() + " created");
+        Diagnosis diagnosis = toEntity(diagnosisDTO);
+        diagnosis.getPatient().setDiagnosis(diagnosis);
+        diagnosis.getMainDisease().addDiagnosis(diagnosis);
+        Diagnosis diagnosis1 = diagnosisRepository.save(diagnosis);
+        logger.log(Level.INFO, "Diagnosis " + diagnosis1.getMainDisease().getName() + " created");
         return toDTO(diagnosis1);
     }
 
     @Override
     public DiagnosisDTO updateDiagnosis(DiagnosisDTO diagnosisDTO) {
-        Diagnosis diagnosis1 = diagnosisRepository.save(toEntity(diagnosisDTO));
-        logger.log(Level.INFO, "Diagnosis " + diagnosis1.getMainDisease().getName() + " updated");
+        Diagnosis diagnosis = toEntity(diagnosisDTO);
+        Diagnosis diagnosis1 = diagnosisRepository.findById(diagnosis.getId()).orElseThrow();
+        diagnosis1.getMainDisease().removeDiagnosis(diagnosis1);
+        diagnosis.getMainDisease().addDiagnosis(diagnosis);
+        Diagnosis diagnosis2 = diagnosisRepository.save(diagnosis);
+        logger.log(Level.INFO, "Diagnosis " + diagnosis2.getMainDisease().getName() + " updated");
         return toDTO(diagnosis1);
     }
 
     @Override
     public void deleteDiagnosis(Long id) {
+        Diagnosis diagnosis = diagnosisRepository.findById(id).orElseThrow();
+        diagnosis.getMainDisease().removeDiagnosis(diagnosis);
         diagnosisRepository.deleteById(id);
         logger.log(Level.INFO, "Diagnosis with id " + id + " deleted");
     }
